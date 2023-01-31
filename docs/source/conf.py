d3c053007827731a41e7579a18539d9049de4457
ashleyjross <ashley.jacob.ross@gmail.com>
2023-01-31 17:18:44 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BAOfit_xs'
+#copyright = '2021, Graziella'
+#author = 'Graziella'
 
-release = '0.1'
-version = '0.1.0'
+#release = '0.1'
+#version = '0.1.0'
 
 # -- General configuration
 
